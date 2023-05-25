da5af75a1afa7723777151d5ac5a03e75bd3864b
alpiansyah1204 <96229888+alpiansyah1204@users.noreply.github.com>
2023-05-26 04:22:14 +0700

    updte

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,jsonify,render_template,send_file
+from flask import Flask,request,send_file
 # from camera import VideoCamera
 import pandas as pd
 from moviepy.editor import *
